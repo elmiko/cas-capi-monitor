acb8044825ec3a46d47d58c9ffb536b46f9c43c1
elmiko <msm@opbstudios.com>
2025-06-13 16:25:39 -0400

    add flags for separate kubeconfigs

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	"github.com/elmiko/cas-capi-monitor/controllers"
+	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	capiv1beta1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -16,10 +21,16 @@ import (
 
 func main() {
 	logf.SetLogger(zap.New())
-
 	log := logf.Log.WithName("cas-capi-monitor")
 
-	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{})
+	var capiKCFile string
+	var nodeKCFile string
+
+	flag.StringVar(&capiKCFile, "ck", "", "path to kubeconfig for the cluster api resources")
+	flag.StringVar(&nodeKCFile, "nk", "", "path to kubeconfig for the node resources")
+	flag.Parse()
+
+	mgr, err := manager.New(getCapiConfigOrDie(capiKCFile, log), manager.Options{})
 	if err != nil {
 		log.Error(err, "could not create manager")
 		os.Exit(1)
@@ -46,7 +57,7 @@ func main() {
 		ControllerManagedBy(mgr).
 		For(&corev1.Node{}).
 		Complete(&controllers.NodeReconciler{
-			Client: mgr.GetClient(),
+			Client: getNodeClientOrDie(nodeKCFile, log),
 		})
 	if err != nil {
 		log.Error(err, "could not create node controller")
@@ -57,4 +68,35 @@ func main() {
 		log.Error(err, "could not start manager")
 		os.Exit(1)
 	}
+}
+
+func getCapiConfigOrDie(filename string, log logr.Logger) *rest.Config {
+	if len(filename) == 0 {
+		return config.GetConfigOrDie()
+	} else {
+		capiConfig, err := clientcmd.BuildConfigFromFlags("", filename)
+		if err != nil {
+			log.Error(err, "unable to build kubeconfig for the cluster api resources")
+			os.Exit(1)
+		}
+		return capiConfig
+	}
+}
+
+func getNodeClientOrDie(filename string, log logr.Logger) client.Client {
+	if len(filename) == 0 {
+		log.Error(nil, "no kubeconfig for node resources specified")
+		os.Exit(1)
+	}
+	nodeConfig, err := clientcmd.BuildConfigFromFlags("", filename)
+	if err != nil {
+		log.Error(err, "unable to build kubeconfig for the node resources")
+		os.Exit(1)
+	}
+	nodeClient, err := client.New(nodeConfig, client.Options{})
+	if err != nil {
+		log.Error(err, "unable to build client for the node resources")
+		os.Exit(1)
+	}
+	return nodeClient
 }
