25b8ce493e97114b08ea75120737918ddb4c9fbf
elmiko <msm@opbstudios.com>
2025-06-13 16:35:48 -0400

    separate managers

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	capiv1beta1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -23,67 +22,62 @@ func main() {
 	logf.SetLogger(zap.New())
 	log := logf.Log.WithName("cas-capi-monitor")
 
-	var capiKCFile string
 	var nodeKCFile string
 
-	flag.StringVar(&capiKCFile, "ck", "", "path to kubeconfig for the cluster api resources")
 	flag.StringVar(&nodeKCFile, "nk", "", "path to kubeconfig for the node resources")
 	flag.Parse()
 
-	mgr, err := manager.New(getCapiConfigOrDie(capiKCFile, log), manager.Options{})
+	capimgr, err := manager.New(config.GetConfigOrDie(), manager.Options{})
 	if err != nil {
-		log.Error(err, "could not create manager")
+		log.Error(err, "could not create cluster api resource manager")
 		os.Exit(1)
 	}
 
-	err = capiv1beta1.AddToScheme(mgr.GetScheme())
+	err = capiv1beta1.AddToScheme(capimgr.GetScheme())
 	if err != nil {
 		log.Error(err, "unable to add scheme")
 		os.Exit(1)
 	}
 
 	err = builder.
-		ControllerManagedBy(mgr).
+		ControllerManagedBy(capimgr).
 		For(&capiv1beta1.Machine{}).
 		Complete(&controllers.MachineReconciler{
-			Client: mgr.GetClient(),
+			Client: capimgr.GetClient(),
 		})
 	if err != nil {
 		log.Error(err, "could not create machine controller")
 		os.Exit(1)
 	}
 
+	nodemgr, err := manager.New(getNodeConfigOrDie(nodeKCFile, log), manager.Options{})
+	if err != nil {
+		log.Error(err, "could not create node resource manager")
+		os.Exit(1)
+	}
 	err = builder.
-		ControllerManagedBy(mgr).
+		ControllerManagedBy(nodemgr).
 		For(&corev1.Node{}).
 		Complete(&controllers.NodeReconciler{
-			Client: getNodeClientOrDie(nodeKCFile, log),
+			Client: nodemgr.GetClient(),
 		})
 	if err != nil {
 		log.Error(err, "could not create node controller")
 		os.Exit(1)
 	}
 
-	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
-		log.Error(err, "could not start manager")
+	if err := capimgr.Start(signals.SetupSignalHandler()); err != nil {
+		log.Error(err, "could not start cluster api resource manager")
+		os.Exit(1)
+	}
+
+	if err := nodemgr.Start(signals.SetupSignalHandler()); err != nil {
+		log.Error(err, "could not start node resource manager")
 		os.Exit(1)
 	}
 }
 
-func getCapiConfigOrDie(filename string, log logr.Logger) *rest.Config {
-	if len(filename) == 0 {
-		return config.GetConfigOrDie()
-	} else {
-		capiConfig, err := clientcmd.BuildConfigFromFlags("", filename)
-		if err != nil {
-			log.Error(err, "unable to build kubeconfig for the cluster api resources")
-			os.Exit(1)
-		}
-		return capiConfig
-	}
-}
-
-func getNodeClientOrDie(filename string, log logr.Logger) client.Client {
+func getNodeConfigOrDie(filename string, log logr.Logger) *rest.Config {
 	if len(filename) == 0 {
 		log.Error(nil, "no kubeconfig for node resources specified")
 		os.Exit(1)
@@ -93,10 +87,5 @@ func getNodeClientOrDie(filename string, log logr.Logger) client.Client {
 		log.Error(err, "unable to build kubeconfig for the node resources")
 		os.Exit(1)
 	}
-	nodeClient, err := client.New(nodeConfig, client.Options{})
-	if err != nil {
-		log.Error(err, "unable to build client for the node resources")
-		os.Exit(1)
-	}
-	return nodeClient
+	return nodeConfig
 }
